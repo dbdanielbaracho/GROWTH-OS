ece70b29f9c39ead596e04b88558451cfc8833d4
dbdanielbaracho <dbdanielbaracho@gmail.com>
2026-09-09 01:05:46 -0300

    fix: close analytics function dollar quote

--- db/migrations/033_metric_analytics_summary.sql
+++ db/migrations/033_metric_analytics_summary.sql
@@ -61,7 +61,7 @@ BEGIN
   GROUP BY sa.id, sa.platform, sa.provider_account_id, sa.handle, mo.metric_name
   ORDER BY sa.platform, sa.handle NULLS LAST, mo.metric_name;
 END;
-$;
+$$;
 
 ALTER FUNCTION growth.list_metric_analytics_summary(uuid,timestamptz,timestamptz)
   OWNER TO growth_migrator;
