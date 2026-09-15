3836b35ad51beb0ccb64c3c6b24299002a75b794
dbdanielbaracho <dbdanielbaracho@gmail.com>
2026-09-14 22:33:48 -0300

    feat: register deployment identity route

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -3,8 +3,10 @@ import { env } from "./config.js";
 import { registerProductionWeb } from "./production-web.js";
 import { registerYoutubeRoutes } from "./youtube-routes.js";
 import { registerInstagramRoutes } from "./instagram-routes.js";
+import { registerDeploymentInfoRoute } from "./deployment-info.js";
 
 const app = buildApp(true);
+await registerDeploymentInfoRoute(app);
 await registerYoutubeRoutes(app);
 await registerInstagramRoutes(app);
 await registerProductionWeb(app);
